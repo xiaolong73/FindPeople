51bb0abff54e49e210a32ff54f187794b85f56da
xiaolong <522112059@qq.com>
2022-02-26 23:14:36 +0800

    update merge_move.sh

--- Yolov3SPP/make_yolo_dataset/merge_move.sh
+++ Yolov3SPP/make_yolo_dataset/merge_move.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cat 2007_train.txt 2012_train.txt > train.txt
-cp train.txt ../data/my_train_data.txt
-cp 2007_test.txt ../data/my_val_data.txt
-rm 20* train.txt
+mv train.txt ../data/my_train_data.txt
+mv 2007_test.txt ../data/my_val_data.txt
+rm 20* 
