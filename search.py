fd64775a68a49e6dc7ac026b17144b84f511fc28
xiaolong <522112059@qq.com>
2022-04-06 16:37:40 +0800

    merge two net

--- search.py
+++ search.py
@@ -4,7 +4,6 @@ from sys import platform
 
 # For Yolov3SPP
 import numpy as np
-
 from Yolov3SPP.models import *
 from Yolov3SPP.build_utils.datasets import *
 from Yolov3SPP.build_utils.utils import *
@@ -122,7 +121,7 @@ def detect(cfg,                     # YoloV3
 
     # Run inference
     t0 = time.time()
-    for i, (path, img, img0, vid_cap) in enumerate(dataloader):
+    for i, (path, img, im0, vid_cap) in enumerate(dataloader):
         t = time.time()
         save_path = str(Path(output) / Path(path).name) # 保存的路径
 
@@ -131,20 +130,106 @@ def detect(cfg,                     # YoloV3
         pred, _ = model(img)
         det = non_max_suppression(pred.float(), conf_thres, nms_thres)[0] # torch.Size([5, 7])
 
+        if det is not None and len(det) > 0:
+            # Rescale boxes from 512 to true image size 映射到原图
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+            # Print results to screen image 1/3 data\samples\000493.jpg: 288x416 5 persons, Done. (0.869s)
+            print('%gx%g ' % img.shape[2:], end='')  # print image size '288x416'
+            for c in det[:, -1].unique():  # 对图片的所有类进行遍历循环
+                n = (det[:, -1] == c).sum()  # 得到了当前类别的个数，也可以用来统计数目
+                if classes[int(c)] == 'person':
+                    print('%g %ss' % (n, classes[int(c)]), end=', ')  # 打印个数和类别'5 persons'
+
+            # Draw bounding boxes and labels of detections
+            # (x1y1x2y2, obj_conf, class_conf, class_pred)
+            count = 0
+            gallery_img = []
+            gallery_loc = []
+            for *xyxy, conf, cls_conf, cls in det: # 对于最后的预测框进行遍历
+                # *xyxy: 对于原图来说的左上角右下角坐标: [tensor(349.), tensor(26.), tensor(468.), tensor(341.)]
+                if save_txt:  # Write to file
+                    with open(save_path + '.txt', 'a') as file:
+                        file.write(('%g ' * 6 + '\n') % (*xyxy, cls, conf))
+
+                # Add bbox to the image
+                label = '%s %.2f' % (classes[int(cls)], conf) # 'person 1.00'
+                if classes[int(cls)] == 'person':
+                    #plot_one_bo x(xyxy, im0, label=label, color=colors[int(cls)])
+                    xmin = int(xyxy[0])
+                    ymin = int(xyxy[1])
+                    xmax = int(xyxy[2])
+                    ymax = int(xyxy[3])
+                    w = xmax - xmin # 233
+                    h = ymax - ymin # 602
+                    # 如果检测到的行人太小了，感觉意义也不大
+                    # 这里需要根据实际情况稍微设置下
+                    if w*h > 500:
+                        gallery_loc.append((xmin, ymin, xmax, ymax))
+                        crop_img = im0[ymin:ymax, xmin:xmax] # HWC (602, 233, 3)
+                        crop_img = Image.fromarray(cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB))  # PIL: (233, 602)
+                        crop_img = build_transforms(reidCfg)(crop_img).unsqueeze(0)  # torch.Size([1, 3, 256, 128])
+                        gallery_img.append(crop_img)
 
 
+            if gallery_img:
+                gallery_img = torch.cat(gallery_img, dim=0)  # torch.Size([7, 3, 256, 128])
+                gallery_img = gallery_img.to(device)
+                gallery_feats = reidModel(gallery_img) # torch.Size([7, 2048])
+                print("The gallery feature is normalized")
+                gallery_feats = torch.nn.functional.normalize(gallery_feats, dim=1, p=2)  # 计算出查询图片的特征向量
+
+                # m: 2
+                # n: 7
+                m, n = query_feats.shape[0], gallery_feats.shape[0]
+                distmat = torch.pow(query_feats, 2).sum(dim=1, keepdim=True).expand(m, n) + \
+                          torch.pow(gallery_feats, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+                # out=(beta∗M)+(alpha∗mat1@mat2)
+                # qf^2 + gf^2 - 2 * qf@gf.t()
+                # distmat - 2 * qf@gf.t()
+                # distmat: qf^2 + gf^2
+                # qf: torch.Size([2, 2048])
+                # gf: torch.Size([7, 2048])
+                distmat.addmm_(1, -2, query_feats, gallery_feats.t())
+                # distmat = (qf - gf)^2
+                # distmat = np.array([[1.79536, 2.00926, 0.52790, 1.98851, 2.15138, 1.75929, 1.99410],
+                #                     [1.78843, 1.96036, 0.53674, 1.98929, 1.99490, 1.84878, 1.98575]])
+                distmat = distmat.cpu().numpy()  # <class 'tuple'>: (3, 12)
+                distmat = distmat.sum(axis=0) / len(query_feats) # 平均一下query中同一行人的多个结果
+                index = distmat.argmin()
+                if distmat[index] < dist_thres:
+                    print('距离：%s'%distmat[index])
+                    plot_one_box(gallery_loc[index], im0, label='find!', color=colors[int(cls)])
+                    # cv2.imshow('person search', im0)
+                    # cv2.waitKey()
 
 
+        print('Done. (%.3fs)' % (time.time() - t))
 
+        if opt.webcam:  # Show live webcam
+            cv2.imshow(weights, im0)
 
+        if save_images:  # Save image with detections
+            if dataloader.mode == 'images':
+                cv2.imwrite(save_path, im0)
+            else:
+                if vid_path != save_path:  # new video
+                    vid_path = save_path
+                    if isinstance(vid_writer, cv2.VideoWriter):
+                        vid_writer.release()  # release previous video writer
 
+                    fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                    width = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                    height = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                    vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (width, height))
+                vid_writer.write(im0)
 
+    if save_images:
+        print('Results saved to %s' % os.getcwd() + os.sep + output)
+        if platform == 'darwin':  # macos
+            os.system('open ' + output + ' ' + save_path)
 
-
-
-
-
-
+    print('Done. (%.3fs)' % (time.time() - t0))
 
 
     if __name__ == '__main__':
